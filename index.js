85a94bd02790dc6d1d0dd2b97b7c16b3574d70c2
pact0 <kuba.kowalski21331@gmail.com>
2021-04-19 13:47:49 +0200

    Get correct commits for all versions

--- index.js
+++ index.js
@@ -36,9 +36,86 @@ const versionFrom = options.versionFrom;
 const versionTo = options.versionTo;
 const file = options.file === undefined ? "CHANGELOG.md" : options.file;
 
+function filterResponse(response) {
+  let other = [];
+  let fix = [];
+  let feature = [];
+  let majorBreakingChanges = [];
+  let incorrect = [];
+
+  response.all.forEach((commit) => {
+    if (!commit.body) return;
+
+    commit.body.split("\r\n").forEach((x) => {
+      if (!x) return;
+
+      let [left, right] = x.split(":");
+      right = right ? right.trim() : "";
+
+      package = left.match(/(\(\w*\))/)
+        ? left.match(/(\(\w*\))/)[1].slice(1, -1)
+        : undefined;
+
+      let hashtagSection = right.match(/(#\d*)/)
+        ? right.match(/(#\d*)/)[1].slice(1)
+        : undefined;
+
+      let record = {
+        message: x,
+        hash: commit.hash,
+        package: package,
+        digits: hashtagSection,
+      };
+
+      if (x.match(/BREAKING CHANGE/)) {
+        majorBreakingChanges.push({
+          ...record,
+          message: right,
+          type: "BREAKING CHANGE",
+        });
+      }
+
+      if (x.match(/^Other /)) {
+        other.push({ ...record, message: right, type: "Other" });
+      } else if (x.match(/^Feature /)) {
+        feature.push({ ...record, message: right, type: "Feature" });
+      } else if (x.match(/^Fix /)) {
+        fix.push({ ...record, message: right, type: "Fix" });
+      } else if (
+        x.match(/^Docs /) ||
+        x.match(/^Internal /) ||
+        x.match(/^Tests /) ||
+        x.match(/^Revert /) ||
+        x.match(/^Release /)
+      ) {
+      } else {
+        // here all the commits that do not match the specified convention
+        // are pushed into incorrect array and later they will be displayed in console
+        incorrect.push(record);
+      }
+    });
+  });
+  return {
+    other: other,
+    fix: fix,
+    feature: feature,
+    majorBreakingChanges: majorBreakingChanges,
+    incorrect: incorrect,
+  };
+}
+
+async function getCommits(path, from, to, file_name) {
+  await git(path)
+    .log({ from: from, to: to, symmetric: false })
+    .then((response) => {
+      let comimtMessages = filterResponse(response);
+      console.log(comimtMessages.fix);
+    });
+}
+
 git(path)
   .tag()
-  .then((res) => {
+  .then(async (res) => {
     const tags = res.split("\n");
     let correctTags = [];
     let flag = false;
@@ -48,9 +125,8 @@ git(path)
       if (tags[i] === versionTo) flag = false;
     }
 
-    for (let i = correctTags.length - 1; i > 0; i--) {
-      const from = correctTags[i];
-      const to = correctTags[i - 1];
-      console.log(from, to);
+    for (let index = correctTags.length - 1; index > 0; index--) {
+      console.log(correctTags, correctTags[index], correctTags[index - 1]);
+      await getCommits(path, correctTags[index - 1], correctTags[index], file);
     }
   });
