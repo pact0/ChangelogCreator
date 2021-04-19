a1a470dc4cf556a74e7625bf2181bc4258ce1cd9
pact0 <kuba.kowalski21331@gmail.com>
2021-04-19 12:33:21 +0200

    Check for correctness of input

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 #!/usr/bin/env node
 const { program } = require("commander");
+let fs = require("fs");
 
-function errorColor(str) {
-  return `\x1b[31m${str}\x1b[0m`;
+function isDir(path) {
+  try {
+    let stat = fs.lstatSync(path);
+    if (stat.isDirectory()) {
+      return path;
+    }
+  } catch (e) {
+    throw new Error("Incorrect path to dirrectory");
+  }
+}
+
+function isVersion(version) {
+  if (version.match(/v\d+.\d+.\d+/)) {
+    return version;
+  } else {
+    throw new Error("Please provide a correct version number");
+  }
 }
 program
-  .requiredOption("-p,--path <path>", "path to a direvtory")
+  .requiredOption("-p,--path <path>", "path to a directory", isDir)
   .requiredOption(
     "-vf,--versionFrom <versionFrom>",
-    "begginign version of changelog"
+    "begginign version of changelog",
+    isVersion
   )
-  .requiredOption("-vt,--versionTo  <versionTo>", "endversion of changelog")
+  .requiredOption(
+    "-vt,--versionTo  <versionTo>",
+    "endversion of changelog",
+    isVersion
+  )
   .configureOutput({
     writeOut: (str) => process.stdout.write(`[OUT] ${str}`),
     writeErr: (str) =>
       process.stdout.write(
         `[ERR] ${str}\ntype -h or --help for more information.\n`
       ),
-    outputError: (str, write) => write(errorColor(str)),
+    outputError: (str, write) => write(`\x1b[31m${str}\x1b[0m`),
   })
   .helpOption("-h, --help", "use to get description of all ")
   .parse();
